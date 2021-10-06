efb4aa9de6bab5caadfcd92eb54cdb326c4f37b9
超级哆啦酱 <403036847@qq.com>
2021-10-07 01:24:51 +0800

    add slot to diy

--- modes/DIYCreateVehicleMode.lua
+++ modes/DIYCreateVehicleMode.lua
@@ -20,7 +20,10 @@ this.onStartMode = function()
     )
 end
 
-this.onUtilCreated = function(canvas)
+this.onUtilCreated = function(root)
+    this.slotModifyBtnTemplate = root.transform:Find("DIYCreateVehicleCanvas/SlotModifyBtn")
+    this.slotModifyBtnTemplate.gameObject:SetActive(false)
+
     this.addRule(1, "d526381d-f6e4-42fc-9f6b-548866c23241", true, nil, 0)
     this.addRule(2, "ee45c582-3a2f-4f50-b674-46bc62edf381", true, 1, 0)
     this.addRule(3, "5c02c009-51bc-4260-b00e-4dbf26d9325e", true, 2, 0)
@@ -38,7 +41,8 @@ end
 this.refreshInteractBtn = function()
     -- 获取配件安装的状态
     --- @type table<string,boolean[]>
-    local equipSlotStatus = {}
+    local slotStatus = {}
+    local slotInfoCache = {}
 
     -- 获取配件的插槽信息
     for index, x in pairs(this.userDefined.rules) do
@@ -48,11 +52,12 @@ this.refreshInteractBtn = function()
 
         local status = {}
 
-        for i = 1, slotInfos.Length do
+        for i = 0, slotInfos.Length - 1 do
             table.insert(status, false)
         end
 
-        equipSlotStatus[rule.ruleGuid] = status
+        slotStatus[rule.ruleGuid] = status
+        slotInfoCache[rule.ruleGuid] = slotInfos
     end
 
     -- 获取配件插槽的占用信息
@@ -61,11 +66,45 @@ this.refreshInteractBtn = function()
         local rule = x
 
         if rule.parentRuleGuid and rule.parentRuleGuid ~= "" then
-            equipSlotStatus[rule.parentRuleGuid][rule.targetSlotIndex + 1] = true
+            slotStatus[rule.parentRuleGuid][rule.targetSlotIndex + 1] = true -- 注意 Lua index 比 c# +1
         end
     end
 
-    print(inspect(equipSlotStatus))
+    for ruleGuid, statusArray in pairs(slotStatus) do
+        for j, status in pairs(statusArray) do
+            if not status then
+                local slotIndex = j - 1
+                local slotOwnerRuleId = ruleGuid
+
+                local iconPos = CSharpAPI.GetSlotPosFromRule(slotOwnerRuleId, slotIndex, this.userDefined)
+
+                local instance =
+                    GameObject.Instantiate(
+                    this.slotModifyBtnTemplate,
+                    this.slotModifyBtnTemplate.transform.parent,
+                    true
+                )
+
+                instance:GetComponent(typeof(CS.ShanghaiWindy.Core.IconScreenPositionCtrl)).worldPos = iconPos
+                instance:GetComponent("Button").onClick:AddListener(
+                    function()
+                        print(
+                            inspect(
+                                {
+                                    slotIndex = slotIndex,
+                                    iconPos = iconPos,
+                                    slotOwnerRuleId = slotOwnerRuleId
+                                }
+                            )
+                        )
+                    end
+                )
+                instance.gameObject:SetActive(true)
+            end
+        end
+    end
+
+    print(inspect(slotStatus))
 end
 
 this.addRule = function(ruleGuid, itemGuid, isMain, parentRuleGuid, targetSlotIndex)
